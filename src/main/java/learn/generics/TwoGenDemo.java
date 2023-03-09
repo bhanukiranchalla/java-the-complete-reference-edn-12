b8bc92c5a8b3117392ffa94d294d25d64ea9de4e
Bhanu Kiran Challa <challabhanukiran@gmail.com>
2023-03-09 11:32:39 +0000

    generics examples - basics

---
+++ src/main/java/learn/generics/TwoGenDemo.java
@@ -0,0 +1,40 @@
+package learn.generics;
+
+// A simple generic class with two type parameters: T and V.
+class TwoGen<T, V> {
+    T ob1;
+    V ob2;
+
+    // Pass the constructor a reference to an object of type T.
+    TwoGen(T o1, V o2) {
+        ob1 = o1;
+        ob2 = o2;
+    }
+
+    // Return ob.
+    T getOb1() { return ob1; }
+    V getOb2() { return ob2; }
+
+    // Show type of T and V
+    void showTypes() {
+        System.out.println("Type of T is " + ob1.getClass().getName());
+        System.out.println("Type of V is " + ob2.getClass().getName());
+    }
+}
+
+// Demonstrate TwoGen
+class TwoGenDemo {
+    public static void main(String[] args) {
+        TwoGen<Integer, String> tgObj = new TwoGen<Integer, String>(88, "Generics");
+
+        // Show the types.
+        tgObj.showTypes();
+
+        // Obtain and show values.
+        int v = tgObj.getOb1();
+        System.out.println("value: " + v);
+
+        String str = tgObj.getOb2();
+        System.out.println("value: " + str);
+    }
+}
